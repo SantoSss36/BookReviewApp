ec478e4f3d2346ecc6e615ad1b722b5a1cbdcce9
SantoSss36 <tiago.maio06@gmail.com>
2025-11-03 10:31:39 -0500

    promises/async-await add

--- router/general.js
+++ router/general.js
@@ -28,12 +28,42 @@ public_users.get('/',function (req, res) {
   return res.send(JSON.stringify(books, null, 4));
 });
 
+/*public_users.get('/', async function (req, res) {
+    axios.get('http://localhost:5000/books').then(
+    (responseBooks)=>{
+        return res.status(200).send(JSON.stringify(responseBooks.data,null , 4));
+      }
+    ).catch(e=>
+      res.status(404).send("cant get books <br>  "+ e)
+      )
+  }); */
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   
     const isbn = req.params.isbn;
     res.send(books[isbn]);
  });
+
+/*public_users.get('/isbn/:isbn', async function (req, res) {
+  // Write your code here
+  let isbn = req.params.isbn;
+
+  try {
+    const response = await axios.get('http://localhost:5000/books');
+
+    if (response.data[isbn]) {
+      return res.status(200).send(JSON.stringify(response.data[isbn], null, 4));
+    } else {
+      return res.status(404).send("No book found with ISBN " + isbn);
+    }
+  } catch (error) {
+    // Handle errors, e.g., network issues or API errors
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});*/
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -55,6 +85,33 @@ public_users.get('/author/:author',function (req, res) {
     }
 });
 
+/*public_users.get('/author/:author', async function (req, res) {
+  // Write your code here
+  let author = req.params.author;
+  let booksByAuthor = [];
+
+  try {
+    // Assuming the API endpoint for getting all books is http://localhost:5000/books
+    const response = await axios.get('http://localhost:5000/books');
+
+    for (let isbn in response.data) {
+      if (response.data[isbn].author == author) {
+        booksByAuthor.push(response.data[isbn]);
+      }
+    }
+
+    if (booksByAuthor.length > 0) {
+      return res.status(200).send(JSON.stringify(booksByAuthor, null, 4));
+    } else {
+      return res.status(404).send("No book found with author " + author);
+    }
+  } catch (error) {
+    // Handle errors, e.g., network issues or API errors
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});*/
+
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   
@@ -74,6 +131,33 @@ public_users.get('/title/:title',function (req, res) {
         res.status(404).json({ message: "Book not found." });
     }
 });
+
+/*public_users.get('/title/:title', async function (req, res) {
+  // Write your code here
+  let title = req.params.title;
+  let booksByTitle = [];
+
+  try {
+    // Assuming the API endpoint for getting all books is http://localhost:5000/books
+    const response = await axios.get('http://localhost:5000/books');
+
+    for (let isbn in response.data) {
+      if (response.data[isbn].title == title) {
+        booksByTitle.push(response.data[isbn]);
+      }
+    }
+
+    if (booksByTitle.length > 0) {
+      return res.status(200).send(JSON.stringify(booksByTitle, null, 4));
+    } else {
+      return res.status(404).send("No book found with title " + title);
+    }
+  } catch (error) {
+    // Handle errors, e.g., network issues or API errors
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});*/
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
